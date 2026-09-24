a8d0fa2500284ef492d9202795d0e2271d270d27
Aruf Maknojiya <arufmak2004@n01-zeus.olympus.ece.tamu.edu>
2026-09-24 13:56:44 -0500

    added back end agents + llm caller

---
+++ server/validation/commands.py
@@ -0,0 +1,10 @@
+
+#srun --job-name=ecen-454 --cpus-per-task=1 --partition=adademic --qos=olympus-academic --pty bash -l
+
+#source /opt/coe/synopsys/vcs/W-2024.09-SP2-4/setup.vcs.sh
+
+#python3 llm_caller_test.py
+
+#python3 run_pipeline.py rtl/
+
+#python3 GDSII_Validation_Agent.py --netlist /path/to/backend/netlist.v --gds /path/to/backend/layout.gds
